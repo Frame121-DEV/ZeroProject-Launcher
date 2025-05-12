42c3c215811a462674662e5b49ca11f7a929f307
Framelpo <framef795@gmail.com>
2025-05-12 18:05:52 +0700

    ud5

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://felixcraft.xyz/ZeroDistro/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://distro.felixcraft.xyz//distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
